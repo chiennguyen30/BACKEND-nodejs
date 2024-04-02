8c9bec73ff7affd0b9c42eb734bb345661e5d4c1
chiennguyen30 <nguyenchien30102001@gmail.com>
2024-04-02 17:22:58 +0700

    cmt 02/04/2024

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,11 @@
-let getHomePage = (req, res) => {
-  return res.render("homePage.ejs");
+import db from "../models/index";
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+    return res.render("homePage.ejs", { data: JSON.stringify(data) });
+  } catch (error) {
+    console.log(error);
+  }
 };
 let getAboutPage = (req, res) => {
   return res.render("test/aboutPage.ejs");
