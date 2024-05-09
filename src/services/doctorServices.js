383445cef39d6727ea6bf470465c0c6956bdc9c7
chiennguyen30 <nguyenchien30102001@gmail.com>
2024-05-09 16:27:55 +0700

    update infor doctor

--- src/services/doctorServices.js
+++ src/services/doctorServices.js
@@ -61,15 +61,33 @@ let getAllDoctorsServices = () => {
 let saveDetailInforDoctor = (inputData) => {
   return new Promise(async (resolve, reject) => {
     try {
-      if (!inputData.doctorId || !inputData.contentHTML || !inputData.contentMarkdown) {
+      if (
+        !inputData.doctorId ||
+        !inputData.contentHTML ||
+        !inputData.contentMarkdown ||
+        !inputData.action
+      ) {
         resolve({ errCode: 1, errMessage: "Missing parameter" });
       } else {
-        await db.Markdown.create({
-          contentHTML: inputData.contentHTML,
-          contentMarkdown: inputData.contentMarkdown,
-          description: inputData.description,
-          doctorId: inputData.doctorId,
-        });
+        if (inputData.action === "CREATE") {
+          await db.Markdown.create({
+            contentHTML: inputData.contentHTML,
+            contentMarkdown: inputData.contentMarkdown,
+            description: inputData.description,
+            doctorId: inputData.doctorId,
+          });
+        } else if (inputData.action === "EDIT") {
+          let doctorMarkdown = await db.Markdown.findOne({
+            where: { doctorId: inputData.doctorId },
+            raw: false,
+          });
+          if (doctorMarkdown) {
+            doctorMarkdown.contentHTML = inputData.contentHTML;
+            doctorMarkdown.contentMarkdown = inputData.contentMarkdown;
+            doctorMarkdown.description = inputData.description;
+            await doctorMarkdown.save();
+          }
+        }
         resolve({
           errCode: 0,
           errMessage: "Save infro doctor succeed!!",
