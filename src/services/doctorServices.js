dda2ab217ff2e86479284eb4c6c80061fcc8ce10
chiennguyen30 <nguyenchien30102001@gmail.com>
2024-05-09 11:18:09 +0700

    convert anh sang base64

--- src/services/doctorServices.js
+++ src/services/doctorServices.js
@@ -88,14 +88,18 @@ let getDetailDoctorByIdServices = (inputId) => {
       } else {
         let data = await db.User.findOne({
           where: { id: inputId },
-          attributes: { exclude: ["password", "image"] },
+          attributes: { exclude: ["password"] },
           include: [
             { model: db.Markdown, attributes: ["description", "contentHTML", "contentMarkdown"] },
             { model: db.Allcode, as: "positionData", attributes: ["valueEn", "valueVi"] },
           ],
-          raw: true,
+          raw: false,
           nest: true,
         });
+        if (data && data.image) {
+          data.image = new Buffer(data.image, "base64").toString("binary");
+        }
+        if (!data) data = {};
         resolve({
           errCode: 0,
           data: data,
